5198ab3857aed3655f3c2c40f501f8ad54edc918
rayanthoney <rmaxey911@gmail.com>
2022-11-10 06:24:00 -0700

    codingphase added a single menu page

---
+++ resources/views/menu/single-menu.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+<div id="single-menu-page">
+    <div class="content-box">
+        <div class="row">
+            <div class="col-md-6">
+                
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
