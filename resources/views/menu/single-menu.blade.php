8f3289187fbd6e192a1f346c1fd7b399651d2222
rayanthoney <rmaxey911@gmail.com>
2022-11-11 21:18:36 -0700

    codsingphase added single menu

--- resources/views/menu/single-menu.blade.php
+++ resources/views/menu/single-menu.blade.php
@@ -2,12 +2,62 @@
 
 @section('content')
 <div id="single-menu-page">
-    <div class="content-box">
-        <div class="row">
-            <div class="col-md-6">
-                
+    <div class="row">
+        <div class="col-sm-8
+        offset-md-1 col-md-10
+        offset-lg-2 col-md-8">
+            <div class="content-box">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1>Burgers</h1>
+                    </div>
+                    <div class="col-md-6 item">
+                        <div class="item">
+                            <div class="title">
+                                <h4>Deluxe Burger</h4>
+                                <span class="price">$9</span>
+                            </div>
+                            <div class="description">
+                                <p>An all beef patty served with lettuce, tomato, onion, pickle, and mayo. Add cheese for ¢ 60</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 item">
+                        <div class="item">
+                            <div class="title">
+                                <h4>Deluxe Burger</h4>
+                                <span class="price">$9</span>
+                            </div>
+                            <div class="description">
+                                <p>An all beef patty served with lettuce, tomato, onion, pickle, and mayo. Add cheese for ¢ 60</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 item">
+                        <div class="item">
+                            <div class="title">
+                                <h4>Deluxe Burger</h4>
+                                <span class="price">$9</span>
+                            </div>
+                            <div class="description">
+                                <p>An all beef patty served with lettuce, tomato, onion, pickle, and mayo. Add cheese for ¢ 60</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 item">
+                        <div class="item">
+                            <div class="title">
+                                <h4>Deluxe Burger</h4>
+                                <span class="price">$9</span>
+                            </div>
+                            <div class="description">
+                                <p>An all beef patty served with lettuce, tomato, onion, pickle, and mayo. Add cheese for ¢ 60</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
+    </div> 
 </div>
 @endsection
