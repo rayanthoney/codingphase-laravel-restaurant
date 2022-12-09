8f1b1634f418a4ce4d5db7487e0679cfee22765d
rayanthoney <rmaxey911@gmail.com>
2022-12-09 08:30:40 -0700

    cleaned up commits

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'fname' => 'Billy',
+            'lname' => 'Smith',
+            'email' => 'billy@gmail.com',
+            'password' => Hash::make('12345678'),
+        ]);
+        
+        DB::table('users')->insert([
+            'fname' => 'Cindy',
+            'lname' => 'Johnson',
+            'email' => 'cindy@gmail.com',
+            'password' => Hash::make('12345678'),
+        ]);
+    }
+}
