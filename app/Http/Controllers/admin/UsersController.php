d5faab5f2cde996d6f61d8da1a02f7877c84eaaa
rayanthoney <rmaxey911@gmail.com>
2022-12-11 06:23:29 -0700

    added UserController

--- app/Http/Controllers/admin/UsersController.php
+++ app/Http/Controllers/admin/UsersController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Role;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UsersController extends Controller
 {
@@ -15,9 +20,25 @@ class UsersController extends Controller
     public function index() {
         return view('admin/users/all');
     }
+    
     public function create() {
         return view('admin/users/create');
     }
+    
+    public function store() {
+        // return request()->all();
+        $user = new User();
+        $user->fname = request('fname');
+        $user->lname = request('lname');
+        $user->email = request('email');
+        $user->password = Hash::make(request('password'));
+        $user->save();
+        $user->roles()->attach(request('role_id'));
+
+
+        return \redirect('/admin/users');
+    }
+    
     public function edit() {
         return view('admin/users/edit');
     }
